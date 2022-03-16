880de724b9d64b386c736aa5cb1ef9a0ed216f16
Monia Bilashi <bmonia2018@gmail.com>
2022-03-16 21:28:37 +0600

    first commit

---
+++ main.js
@@ -0,0 +1,10 @@
+alert("Welcom To E-School Website");
+
+function myFunction() {
+    var x = document.getElementById("myLinks");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
